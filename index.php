35297e26e149ac27cff59807e0861ff27ba9f087
memtech3 <20732350+memtech3@users.noreply.github.com>
2019-09-27 17:24:26 -0500

    testing navbar

--- index.php
+++ index.php
@@ -1,9 +1,13 @@
 <!DOCTYPE html>
 <html>
+<head>
+<link rel="stylesheet" href="bootstrap.min.css">
+<script src="bootstrap.bundle.min.js">
+<title><title>
+</head>
 <body>
-
+<?php include("includes/navbar.php"); ?>
 <h1>My first PHP page</h1>
-
 <?php
 echo "Hello World!, Hello PHP!";
 ?> 
