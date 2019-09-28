502bcabcdb7683e9b60dcf39b026229d06907ffc
memtech3 <20732350+memtech3@users.noreply.github.com>
2019-09-27 22:37:06 -0500

    fixed popper.js

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <?php include("head.php"); ?>
 </head>
 <body>
-<?php include("navbar.html"); ?>
+<?php include("navbar.php"); ?>
 <h1>My first PHP page</h1>
 <?php
 echo "Hello World!, Hello PHP!";
