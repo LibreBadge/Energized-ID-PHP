129300046ebe73c486d5f1871738e388f52feb6c
Micah Guttman <memtech3@gmail.com>
2019-10-15 14:56:08 -0500

    Update production.php

--- production.php
+++ production.php
@@ -2,18 +2,14 @@
 <html>
 <head>
 <?php include("head.php"); ?>
-<link rel="stylesheet" type="text/css" href="card/style.css"></head>
 <body>
 <?php
   $ID = "1659616";
   $NAME = "Cookie Monster";
 ?>
-<div class="row">
 <?php include("navbar.php"); ?>
-</div>
-<div class="row">
+<br>
 <?php include("card.php"); ?> 
-</div>
 
 </body>
 </html>
