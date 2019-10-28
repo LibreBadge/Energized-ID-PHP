0ced8de550db369b12b6b3b1b4252fb8e2516a0c
Micah Guttman <memtech3@gmail.com>
2019-10-28 14:56:24 -0500

    expiramenting

--- production.php
+++ production.php
@@ -2,14 +2,28 @@
 <html>
 <head>
 <?php include("head.php"); ?>
+<link rel="stylesheet" href="production.css">
+</head>
 <body>
 <?php
   $ID = "1659616";
   $NAME = "Cookie Monster";
 ?>
 <?php include("navbar.php"); ?>
-<br>
-<?php include("card.php"); ?> 
+<div class="container">
+  <div class="row">
+    <div class="col-sm">
+    <div id="card">
+    <?php include("card.php"); ?> 
+    </div>
+    </div>
+    <div class="col-sm">
+      One of three columns
+    </div>
+    <div class="col-sm">
+    </div>
+  </div>
+</div>
 
 </body>
 </html>
