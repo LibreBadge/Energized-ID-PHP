0d82da987b690de15c786b41636601162cdb4ba0
memtech3 <20732350+memtech3@users.noreply.github.com>
2019-11-07 20:32:30 -0600

    404

--- views/404.php
+++ views/404.php
@@ -1,10 +1,17 @@
 <?php include("head.php"); ?>
 <link rel="stylesheet" type="text/css" href="resources/errorCSS/error.css">
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+  <a class="navbar-brand" href="/">Energized-ID</a>
+  
+</nav>
+<br>
+<br>
 
-<div class="jumbotron mx-auto">
+
+<div class="col">
   <h1 class="display-4 mx-auto">404</h1>
   <h1 class="display-4">Hello, looks like you got lost!</h1>
-  <hr class="my-4">
+  <br>
   <div class="row">
       <div class="col-md-auto">
         <button class="btn btn-primary btn-lg" onclick="goBack()">Go Back</button>
