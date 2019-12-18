2fb0b30b02b072bdaf7a0c2b462074f02483df3b
Micah Guttman <memtech3@gmail.com>
2019-12-18 10:53:38 -0600

    Update database.php

--- database.php
+++ database.php
@@ -15,7 +15,7 @@ try {
     if ($result->num_rows > 0) {
         // output data of each row
         while($row = $result->fetch_assoc()) {
-            echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+            echo "id: " . $row["id"]. " - Name: " . $row["firstName"]. " " . $row["lastName"]. "<br>";
         }
     } else {
         echo "0 results";
