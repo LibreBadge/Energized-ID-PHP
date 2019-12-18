f2fb959de867ea9e80d29d3aef0d469348421b41
Micah Guttman <memtech3@gmail.com>
2019-12-18 11:07:22 -0600

    database

---
+++ database2.php
@@ -0,0 +1,27 @@
+<?php
+//this is a test database set up for testing purposes on energized-id-dev.memtech3.com . DO NOT USE THIS IN A PRODUCTION ENVIRONMENT
+$servername = "localhost";
+$username = "u952595018_ID";
+$password = "admin1";
+$dbname = "u952595018_ID";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT * FROM Cardholders";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "id: " . $row["id"]. " - Name: " . $row["firstName"]. " " . $row["lastName"]. "<br>";
+    }
+} else {
+    echo "0 results";
+}
+$conn->close();
+?>
--- index.php
+++ index.php
@@ -18,6 +18,9 @@ switch ($request) {
     case '/database':
         require __DIR__ . '/database.php';
         break;
+    case '/database2':
+        require __DIR__ . '/database2.php';
+        break;
     default:
         http_response_code(404);
         require __DIR__ . '/views/404.php';
