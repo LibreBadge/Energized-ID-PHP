5c62c2be2cc862198e16e63dabe7ffec7e02e909
Micah Guttman <memtech3@gmail.com>
2019-12-18 10:53:00 -0600

    Update database.php

--- database.php
+++ database.php
@@ -12,8 +12,15 @@ try {
     echo "Connected successfully";
     $sql = "SELECT id, firstName, lastName FROM Cardholders WHERE lastName='Doe'";
     $result = $conn->query($sql);
-    echo $result;
+    if ($result->num_rows > 0) {
+        // output data of each row
+        while($row = $result->fetch_assoc()) {
+            echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+        }
+    } else {
+        echo "0 results";
     }
+}
 catch(PDOException $e)
     {
     echo "Connection failed: " . $e->getMessage();
