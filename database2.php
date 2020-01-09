09a7c70f57fbfb44a65a3063836806278c0e6001
Micah Guttman <memtech3@gmail.com>
2020-01-09 13:10:05 -0600

    Update database2.php

--- database2.php
+++ database2.php
@@ -13,7 +13,7 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT * FROM `$table` WHERE `lastName` = 'Doe'";
+$sql = "SELECT * FROM $table WHERE `lastName` = 'Doe'";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
