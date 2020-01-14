3ddf49904f9657aa41097789da0e261df51fe2f1
Micah Guttman <memtech3@gmail.com>
2020-01-14 10:02:30 -0600

    Update database2.php

--- database2.php
+++ database2.php
@@ -1,29 +1,31 @@
-1
 <?php
 //this is a test database set up for testing purposes on energized-id-dev.memtech3.com . DO NOT USE THIS IN A PRODUCTION ENVIRONMENT
 $servername = "localhost";
 $username = "u952595018_ID";
 $password = "admin1";
 $dbname = "u952595018_ID";
-$table = "Cardholders"
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-    echo "Connection failed"
-}
 
-$sql = "SELECT * FROM '$table' WHERE `lastName` = 'Doe'";
-$result = $conn->query($sql);
-
-if ($result->num_rows > 0) {
-    // output data of each row
-    while($row = $result->fetch_assoc()) {
-        echo "id: " . $row["id"]. " - Name: " . $row["firstName"]. " " . $row["lastName"]. "<br>";
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname;", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
+    $sql = "SELECT * FROM `Cardholders`;"
+    $result = $conn->query($sql);
+    if (true/*$result->num_rows > 0*/) {
+        // output data of each row
+        while($row = $result->fetch_assoc()) {
+            echo "id: " . $row["id"]. " - Name: " . $row["firstName"]. " " . $row["lastName"]. " " . $row["securityLevel"]. "<br>";
+        }
+    } 
+    else {
+        echo "0 results :/";
     }
-} else {
-    echo "0 results";
 }
-$conn->close();
+catch(PDOException $e)
+    {
+    echo "Connection failed: " . $e->getMessage();
+    }
+
+
 ?>
